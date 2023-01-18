dca95832600e941ac02edd2c58d03f95b564759f
Giap <ryon@conjurers.dev>
2023-01-18 17:30:49 +0800

    - Complete Setting View

--- Avocados/Views/SettingsView.swift
+++ Avocados/Views/SettingsView.swift
@@ -8,11 +8,87 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    //MARK: - PROPERTIES
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = true
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Settings")
+        
+        VStack(alignment: .center, spacing: 0, content: {
+            //MARK: - HEADER
+            VStack(alignment: .center, spacing: 5, content: {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            }) //: End of VStack
+            .padding()
+            
+            Form {
+                //MARK: - SECTION #1
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotification, label: {
+                        Text("Enable Notifications")
+                    })
+                    
+                    Toggle(isOn: $backgroundRefresh, label: {
+                        Text("Background Refresh")
+                    })
+                }
+                
+                //MARK: - SECTION #2
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        HStack {
+                            Text("Compatibility")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Iphone & Ipad")
+                        }
+                        HStack {
+                            Text("Developer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Giap")
+                        }
+                        HStack {
+                            Text("Version")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("1.0.0")
+                        }
+                    } else {
+                        HStack {
+                            Text("Personal Message")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Happy Coding 👍")
+                        }
+                    }
+                    
+                }
+            }
+        })
+        .frame(maxWidth: 640)
     }
 }
 
+//MARK: - PREVIEW
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
