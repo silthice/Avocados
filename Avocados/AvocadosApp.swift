3e1c3b1c94e4752e344e3afa546fccf054797e5f
Giap <ryon@conjurers.dev>
2023-01-18 12:26:05 +0800

    -Initial setups

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Giap on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
